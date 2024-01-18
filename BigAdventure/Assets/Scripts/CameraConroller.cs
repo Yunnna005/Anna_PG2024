d11f440fcecb3b1d638e16ab2eafa3ad0a017e58
t00243622 <annakovalenko0500@gmail.com>
2024-01-18 15:57:40 +0000

    Created CameraController Script. Added rotation for Camera and rotation should be fixed for player Signed-off-by: t00243622 <annakovalenko0500@gmail.com>

--- BigAdventure/Assets/Scripts/CameraConroller.cs
+++ BigAdventure/Assets/Scripts/CameraConroller.cs
@@ -4,16 +4,61 @@ using UnityEngine;
 
 public class CameraConroller : MonoBehaviour
 {
+    //ChatGPT helped with logic. The code is mine.
+
     public GameObject player;
+    public Camera camera_view1;
+    public Camera camera_view3;
+
+    //private Vector3 _offset_camera_view1 = new Vector3(0, 1.053f, 0.053f);
+    //private Vector3 _offset_camera_view3 = new Vector3(-0.057f, 1.994f, -2.178f);
+
+    private Vector3 _orbit_camera_view1;
+    private Vector3 _orbit_camera_view3;
+    private float _turnSpeed = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera_view1.enabled = false;
+
+        _orbit_camera_view1 = new Vector3(player.transform.position.x, player.transform.position.y + 1.053f, player.transform.position.z + 0.2f);
+        _orbit_camera_view3 = new Vector3(player.transform.position.x, player.transform.position.y + 3.0f, player.transform.position.z - 3.5f);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + new Vector3(0,3,-2);
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SwitchToCamera1();   
+        }else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SwitchToCamera3();
+        }
+
+        UpdateCameraPOsition();
+    }
+
+    void SwitchToCamera1()
+    {
+        camera_view1.enabled = true;
+        camera_view3.enabled = false;
+    }
+
+    void SwitchToCamera3()
+    {
+        camera_view1.enabled = false;
+        camera_view3.enabled = true;
+    }
+
+    void UpdateCameraPOsition()
+    {
+        _orbit_camera_view3 = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * _turnSpeed, Vector3.up) * _orbit_camera_view3;
+        _orbit_camera_view1 = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * _turnSpeed, Vector3.up) * _orbit_camera_view1;
+
+        camera_view1.transform.position = player.transform.position  + _orbit_camera_view1;
+        //camera_view1.transform.LookAt(player.transform.position);
+        camera_view3.transform.position = player.transform.position  + _orbit_camera_view3;
+        camera_view3.transform.LookAt(player.transform.position);
     }
 }
--- BigAdventure/Assets/Scripts/PlayerContoller.cs
+++ BigAdventure/Assets/Scripts/PlayerContoller.cs
@@ -8,10 +8,17 @@ public class PlayerContoller : MonoBehaviour
     float leftRightMove;
     float backForwardMove;
     private float _speed = 5;
+    private float _jumpForce = 300.0f;
+    private bool _isOnGround = true;
+    private float _gravityModifier = 1.2f;
+
+    Rigidbody playerRigitbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigitbody = GetComponent<Rigidbody>();
+
+        Physics.gravity *= _gravityModifier;
     }
 
     // Update is called once per frame
@@ -24,5 +31,19 @@ public class PlayerContoller : MonoBehaviour
         //Move left or right
         leftRightMove = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * _speed * leftRightMove);
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isOnGround)
+        {
+            playerRigitbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            _isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isOnGround = true;
+        }
     }
 }
