224259684b507f19e8d5c5c002699a91c2ed3592
STUDENT Anna Kovalenko <Anna.Kovalenko@students.ittralee.ie>
2024-01-23 16:16:54 +0000

    Signed-off-by: STUDENT Anna Kovalenko <Anna.Kovalenko@students.ittralee.ie>

--- BigAdventure/Assets/Scripts/PlayerContoller.cs
+++ BigAdventure/Assets/Scripts/PlayerContoller.cs
@@ -30,20 +30,31 @@ public class PlayerContoller : MonoBehaviour
         //Move Back or Forward
         backForwardMove = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * _speed * Time.deltaTime * backForwardMove);
-        //if(backForwardMove > 0 )
-        //{
-        //    animator.GetCurrentAnimatorStateInfo(0).IsName("RunBackForward");
-        //}
+        animator.SetFloat("RunBackForward", backForwardMove);
+        animator.SetBool("isRunningBackForward", backForwardMove > 0 || backForwardMove < 0);
 
         //Move left or right
         leftRightMove = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * _speed * leftRightMove);
+        animator.SetFloat("runLeftRight", leftRightMove);
+        animator.SetBool("isRunningLeftRight", leftRightMove > 0 || leftRightMove < 0);
 
         if (Input.GetKeyDown(KeyCode.Space) && _isOnGround)
         {
             playerRigitbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
             _isOnGround = false;
+            animator.SetBool("isJumping", true);
         }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animator.SetBool("isAttacking", true);
+        }
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -51,6 +62,7 @@ public class PlayerContoller : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             _isOnGround = true;
+            animator.SetBool("isJumping", false);
         }
     }
 
