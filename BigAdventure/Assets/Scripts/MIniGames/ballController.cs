b6733244cfc5266510abc85b2210dbaa82b3fae6
STUDENT Anna Kovalenko <Anna.Kovalenko@students.ittralee.ie>
2024-02-27 15:39:58 +0000

    Working on miniGame. Signed-off-by: STUDENT Anna Kovalenko <Anna.Kovalenko@students.ittralee.ie>

---
+++ BigAdventure/Assets/Scripts/MIniGames/ballController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballController : MonoBehaviour
+{
+    //public Basketball_miniGame basketball_MiniGameScript;
+    
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Ground"))
+    //    {
+    //        basketball_MiniGameScript.OnBallHitFloor();
+    //    }
+    //}
+}
